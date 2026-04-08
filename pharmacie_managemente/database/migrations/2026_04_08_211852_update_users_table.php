f77e53c499f80934a62cdaf6fdb58037341f38a5
aitbhou2003 <abdellahaitbhou@gmail.com>
2026-04-08 22:20:57 +0100

    modifier le tableau de users

---
+++ pharmacie_managemente/database/migrations/2026_04_08_211852_update_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn('name');
+            $table->foreignId('role_id')->constrained()->after('id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('telephone')->nullable();
+            $table->boolean('is_actif')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};

a848d48a061b8d20987f721da4b1d2d585008a3f
aitbhou2003 <abdellahaitbhou@gmail.com>
2026-04-08 22:34:59 +0100

    ajouter la megration de stocke_vente

---
+++ pharmacie_managemente/database/migrations/2026_04_08_213229_create_stock_ventes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock_ventes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('vente_id')->constrained('ventes')->onDelete('cascade');
+            $table->foreignId('stock_id')->constrained('stocks');
+            $table->integer('quantite');
+            $table->decimal('prix_unitaire', 10, 2); // snapshot du prix au moment de la vente
+            $table->decimal('total', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock_ventes');
+    }
+};

5f8ccf65f86f34b68b75c641ae896da517a837d1
aitbhou2003 <abdellahaitbhou@gmail.com>
2026-04-09 00:58:05 +0100

    ajouter le seederde role et de user un admin et employer pour le test

---
+++ pharmacie_managemente/database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+         User::create([
+            'firstname' => 'Admin',
+            'lastname' => 'Pharmacien',
+            'email' => 'admin@pharmacie.com',
+            'password' => bcrypt('password'),
+            'role_id' => 1, // admin (créé par RoleSeeder)
+            'telephone' => '0612345678',
+            'is_actif' => true,
+        ]);
+
+         User::create([
+            'firstname' => 'Jean',
+            'lastname' => 'Employe',
+            'email' => 'employe@pharmacie.com',
+            'password' => bcrypt('password'),
+            'role_id' => 2, // employe
+            'telephone' => '0698765432',
+            'is_actif' => true,
+        ]);
+    }
+
+}

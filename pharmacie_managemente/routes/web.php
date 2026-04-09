0f8fe38703b9028f0d7840bf69275b43678e9a6d
aitbhou2003 <abdellahaitbhou@gmail.com>
2026-04-09 17:03:01 +0100

    ajouter les route necessaire

--- pharmacie_managemente/routes/web.php
+++ pharmacie_managemente/routes/web.php
@@ -1,15 +1,43 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
+// login page
 Route::get('/login', [LoginController::class, 'index'])
     ->name('login');
 
-Route::post('/login',[LoginController::class,'store'])
-->name('login');
+// login store
+Route::post('/login', [LoginController::class, 'store'])
+    ->name('login');
+
+
+
+// logout
+Route::post('/logout', [LogoutController::class, 'logout'])
+    ->name('logout');
+
+// admin dashboared
+Route::get('/admin/dashboard', function () {
+    return view('dashboard.admin');
+})->name('dashboard.admin')
+    ->middleware('role:admin');
+
+// empluye dashboared  
+Route::get('/employe/dashboard', function () {
+    return view('dashboard.employe');
+})->name('dashboard.employe');
+
+Route::middleware(['role:admin'])->group(function () {
+    Route::get('/register', [RegisterController::class, 'index'])
+        ->name('register');
+
+    Route::post('/register', [RegisterController::class, 'store'])
+        ->name('register.store');
+});

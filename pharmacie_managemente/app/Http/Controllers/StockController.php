7195b0ba7609c3dac5eb9b7efe8926c7ce2a7ab0
aitbhou2003 <abdellahaitbhou@gmail.com>
2026-04-14 14:44:54 +0100

    ajouter controller de stck pour ajouter un stock d'un ancieene medicament et modifier quantiter d un stock

---
+++ pharmacie_managemente/app/Http/Controllers/StockController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MovementStock;
+use App\Models\Stock;
+use Illuminate\Http\Request;
+
+class StockController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $srocks = Stock::with('medicament')
+            ->where('is_actif', true)
+            ->orderBy('date_expiration')
+            ->paginate('15');
+
+        return view('stocks.index', compact('stocks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Request $request)
+    {
+        //
+        $validated = $request->validate([
+            'medicament_id' => ['required', 'exists:medicaments,id'],
+            'numero_lot' =>  ['required', 'string'],
+            'quantite' => ['required', 'integer', 'min:1'],
+            'seuil_minimum' => ['required', 'integer', 'min:1'],
+            'prix_achat' => ['required', 'numeric', 'min:0'],
+            'prix_vente' => ['required', 'numeric', 'min:0'],
+            'date_expiration' => ['required', 'date', 'after:today'],
+        ]);
+
+        $stock = Stock::create($validated + ['is_actif' => true]);
+        MovementStock::create([
+            'stock_id' => $stock->id,
+            'user_id' => auth()->id(),
+            'type' => 'entree',
+            'quantite' => $validated['quantite'],
+            'quantite_avant' => 0,
+            'quantite_apres' => $validated['quantite'],
+            'motif' => 'Réception nouvelle commande',
+        ]);
+
+        return redirect()->back()
+            ->with('success', 'Stock ajouté avec succès');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Stock $stock)
+    {
+        //
+        $validated = $request->validate([
+            'quantite_ajustee' => ['required', 'numeric', 'min:0'],
+            'motif' => ['required', 'string'],
+        ]);
+
+        $ancienne = $stock->quantite;
+        $nouvelle =  $validated['quantite_ajustee'];
+
+        $stock->update(['quantite' => $nouvelle]);
+        $type = 'sortie';
+        if ($nouvelle > $ancienne) {
+            $type = 'entree';
+        }
+
+        MovementStock::create([
+            'stock_id' => $stock->id,
+            'user_id' => auth()->id(),
+            'type' => 'ajustement',
+            'quantite' => abs($nouvelle - $ancienne),
+            'quantite_avant' => $ancienne,
+            'quantite_apres' => $nouvelle,
+            'motif' => $validated['motif'],
+        ]);
+
+        return redirect()->back()
+            ->with('success', 'Stock ajusté');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
